14d4073526a3889e8c2fc5d02855cad03bf8c821
mendefrost <Mende_.12@hotmail.com>
2020-06-26 13:34:10 +1000

    added forms

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <form className="app-form add-box-shadow add-border-radius ">
+        <h1 className="app-form-title">
+          <FontAwesomeIcon icon="user" /> Sign up
+        </h1>
+        <input className="app-form-input " placeholder="Name" type="text" />
+
+        <input className="app-form-input " placeholder="Email" type="text" />
+
+        <input
+          className="app-form-input"
+          placeholder="Password"
+          type="password"
+        />
+        <button className="btn-form-button"> Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
